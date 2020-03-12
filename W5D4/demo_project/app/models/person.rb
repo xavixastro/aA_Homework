54efb5e000b8966c75b408eb04a848ca9541dc87
javier <xavixastro@gmail.com>
2020-03-12 07:11:17 -0700

    Create first rails project

---
+++ W5D4/demo_project/app/models/person.rb
@@ -0,0 +1,11 @@
+class Person < ApplicationRecord
+    validates :name, presence:true
+    validates :house_id, presence:true
+
+    belongs_to(:house, {
+        primary_key: :id, #House id
+        foreign_key: :house_id,
+        class_name: :House
+    })
+    
+end

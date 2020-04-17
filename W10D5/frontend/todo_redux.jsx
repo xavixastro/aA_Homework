0923542d1373aabe443b4790160916601b8b5ba0
javier <xavixastro@gmail.com>
2020-04-17 00:36:58 -0700

    Middleware hw

---
+++ W10D5/frontend/todo_redux.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+// const addLoggingToDispatch = (store) => {
+//   let dispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     dispatch(action);
+//     store.getState()
+//   }
+// }
+
+// const addLoggingToDispatch = (store) => {
+//   return (next) => {
+//     return (action) => {
+//       console.log(store.getState());
+//       console.log(action);
+//       next(action);
+//       console.log(store.getState());
+//     }
+//   }
+// }
+
+// const applyMiddlewares = (store, middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach(middleware => {
+//     dispatch = middleware(store)(dispatch);
+//   })
+//   return Object.assign({}, store, { dispatch })
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+  JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+  // store.dispatch = addLoggingToDispatch(store);
+  // store = applyMiddlewares(store, [addLoggingToDispatch]);
+
+
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
